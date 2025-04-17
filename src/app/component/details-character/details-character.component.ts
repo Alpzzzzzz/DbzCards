a9c1f98d90729e22529af45200f3838e5a1e3765
Alpzzzzzz <of.alpz@gmail.com>
2025-04-17 12:10:03 -0700

    Detalles del Personaje

--- src/app/component/details-character/details-character.component.ts
+++ src/app/component/details-character/details-character.component.ts
@@ -1,17 +1,33 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ServicioService } from '../../servicio.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-details-character',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './details-character.component.html',
-  styleUrl: './details-character.component.css'
+  styleUrl: './details-character.component.css',
+  providers: [ServicioService],
 })
-export class DetailsCharacterComponent {
+export class DetailsCharacterComponent implements OnInit{
   route: ActivatedRoute = inject(ActivatedRoute);
   charactedId!: number;
+  data: any;
+  transformations: any;
 
-  constructor(){
-    this.charactedId = Number(this.route.snapshot.params);
+  constructor(private request: ServicioService){
+    this.charactedId = Number(this.route.snapshot.paramMap.get('id'));
+  }
+
+  ngOnInit(): void {
+    this.loadData(this.charactedId);
+  }
+
+  loadData(id: number): void {
+    this.request.getCharacter(id).subscribe((element:any) => {
+      this.data = element;
+      console.warn(this.data.transformations);
+    })
   }
 }
